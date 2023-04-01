e3cecb4532f762ffa3f09f19b2c41cb4faa3cd25
drSilence88 <eduardo.mexa@hotmail.com>
2023-03-31 19:19:45 -0600

    Properties for port and default name for the URL

--- src/main/java/com/luv2tocidespringboot/demo/mycoolapp/rest/FunRestController.java
+++ src/main/java/com/luv2tocidespringboot/demo/mycoolapp/rest/FunRestController.java
@@ -1,11 +1,28 @@
 package com.luv2tocidespringboot.demo.mycoolapp.rest;
 
+import java.lang.annotation.Target;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestController {
 	
+	//inject properties for: coach.name and team.name
+	
+	@Value("${coach.name}")
+	private String coachName;
+	
+	@Value("${team.name}")
+	private String teamName;
+	
+	//expose new endpoint for "teaminfo"
+	@GetMapping("/teaminfo")
+	public String getTeaminfo() {
+		return "Coach: " + coachName + ", Team name: " + teamName;
+	}
+
 	//expose "/" Hello World
 	@GetMapping("/")
 	public String sayHello() {
